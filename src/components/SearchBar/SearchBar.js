4479a5bb1c3fb7a6819591026ddddce277533b7f
Richard Ball <webalation@gmail.com>
2020-04-23 11:22:53 -0400

    Finalized renderSortByOptions method, used to dynamically create the list items needed to display the sort options.

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions {
+	'Best Match': 'best_match',
+	'Highest Rated': 'rating',
+	'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+	renderSortByOptions() {
+		return Object.keys(sortByOptions).map(sortByOption => {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return <li key={sortByOptionValue}>{sortByOption}</li>;
+		});
+	}
+};
