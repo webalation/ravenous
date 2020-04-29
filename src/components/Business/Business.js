151f5b73458a65634fe9180030c9d0269531c05b
Richard Ball <webalation@gmail.com>
2020-04-29 09:17:54 -0400

    Removed business object and accessed information via this.props.

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import './Business.css';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
 class Business extends React.Component {
 	render() {
 		return (
 			<div className="Business">
   				<div className="image-container">
-    					<img src={business.imageSrc} alt={business.name}/>
+    					<img src={this.props.business.imageSrc} alt={this.props.business.name}/>
   				</div>
   					<h2>MarginOtto Pizzeria</h2>
   				<div className="Business-information">
     				<div className="Business-address">
-      					<p>{business.address}</p>
-      					<p>{business.city}</p>
-      					<p>{business.state} {business.zipCode}</p>
+      					<p>{this.props.business.address}</p>
+      					<p>{this.props.business.city}</p>
+      					<p>{this.props.business.state} {this.props.business.zipCode}</p>
     				</div>
     				<div className="Business-reviews">
-      					<h3>{business.category}</h3>
+      					<h3>{this.props.business.category}</h3>
       					<h3 className="rating">4.5 stars}</h3>
-      					<p>{business.reviews} reviews</p>
+      					<p>{this.props.business.reviews} reviews</p>
     				</div>
   				</div>
 			</div>
