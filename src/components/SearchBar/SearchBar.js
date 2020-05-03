167a298b4d69164582d7514b045250fd3196db63
Richard Ball <webalation@gmail.com>
2020-05-03 12:02:21 -0400

    Completed handleSortByChange and getSortByClass methods. Active text now available on filter menu.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-	'Best Match': 'best_match',
-	'Highest Rated': 'rating',
-	'Most Reviewed': 'review_count'
-};
-
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match'
+		}
+
+		this.sortByOptions = {
+			'Best Match': 'best_match',
+			'Highest Rated': 'rating',
+			'Most Reviewed': 'review_count'
+		}
+	}
+
+	getSortByClass(sortByOption) {
+		return	this.state.sortBy === sortByOption
+				? 'active'
+				: ''
+	}
+
+	handleSortByChange(sortByOption) {
+		this.setState({
+			sortBy: sortByOption
+		})
+	}
+
 	renderSortByOptions() {
-		return Object.keys(sortByOptions).map(sortByOption => {
-			let sortByOptionValue = sortByOptions[sortByOption];
-			return <li key={sortByOptionValue}>{sortByOption}</li>;
+		return Object.keys(this.sortByOptions).map(sortByOption => {
+			let sortByOptionValue = this.sortByOptions[sortByOption];
+			return <li className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>;
 		});
 	}
+
 	render() {
 		return (
 			<div className="SearchBar">
