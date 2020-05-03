34da3ff41928a1593d2ecac3ea751b3671e1f83a
Richard Ball <webalation@gmail.com>
2020-05-03 17:31:31 -0400

    Added visual feedback for the sorting options at the top of the search bar; Set the state of sorting options and input elements; Simulated a search query with the Lets Go button.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,12 +4,15 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			term: '',
 			location: '',
 			sortBy: 'best_match'
 		}
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+
 
 		this.sortByOptions = {
 			'Best Match': 'best_match',
@@ -30,6 +33,23 @@ class SearchBar extends React.Component {
 		})
 	}
 
+	handleTermChange(event) {
+		this.setState({
+			term: event.target.value
+		});
+	}
+
+	handleLocationChange(event) {
+		this.setState({
+			location: event.target.value
+		});
+	}
+
+	handleSearch(event) {
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		event.preventDefault();
+	}
+
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map(sortByOption => {
 			let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -46,11 +66,11 @@ class SearchBar extends React.Component {
     				</ul>
   				</div>
   				<div className="SearchBar-fields">
-   					<input placeholder="Search Businesses" />
-    				<input placeholder="Where?" />
+   					<input placeholder="Search Businesses" onChange={this.handleTermChange} />
+    				<input placeholder="Where?" onChange={this.handleLocationChange} />
   				</div>
   				<div className="SearchBar-submit">
-    				<a href="#">Let's Go</a>
+    				<a onClick={this.handleSearch} href="#">Let's Go</a>
   				</div>
 			</div>
 		);
