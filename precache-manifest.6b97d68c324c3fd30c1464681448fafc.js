6b58de018e5a39f7bbcc3f9cbe07a1b712780eaf
Richard Ball <webalation@gmail.com>
2020-05-15 14:51:01 -0400

    Updates

---
+++ precache-manifest.6b97d68c324c3fd30c1464681448fafc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb51b04848729088be8d1490c5a24a8e",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "760d2ea51cce8d9e6976",
+    "url": "/ravenous/static/css/main.0de32da1.chunk.css"
+  },
+  {
+    "revision": "f964c4e3ab7a514ae181",
+    "url": "/ravenous/static/js/2.2d239d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.2d239d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760d2ea51cce8d9e6976",
+    "url": "/ravenous/static/js/main.51772b0d.chunk.js"
+  },
+  {
+    "revision": "9632db665afb2db74baa",
+    "url": "/ravenous/static/js/runtime-main.347f58c1.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
